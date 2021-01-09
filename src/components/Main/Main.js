f1cd28e42a7ddfcb55d4f56e3540da720e0088fe
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 21:43:42 +0100

    Add & export component Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,2 +1,13 @@
 import React, { Component } from 'react';
 import './Main.css';
+
+class Main extends Component{
+  render(){
+    return(
+      <div>
+      </div>
+    );
+  }
+}
+
+export default Main;
