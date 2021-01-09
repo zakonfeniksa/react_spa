de7c66e4e50bbc348dd74b5047bc613c523aa5b7
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 21:59:26 +0100

    Add & Export component Contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,2 +1,15 @@
 import React, { Component } from './Contact.css';
 import './Contact.css';
+
+class Contact extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Masz pytania?</h2>
+        <p>Najlepiej wyślij wiadomość przez nasz mail</p>
+      </div>
+    );
+  }
+}
+
+export default Contact;
