e81d3e3f59b032fdd684b80e556c86151f092f3a
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 21:44:35 +0100

    import & use component Main

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Main from './components/Main/Main.js';
 
 ReactDOM.render(
-  <div>Nic tu nie ma!</div>,
+  <Main />,
   document.getElementById("root")
 );
