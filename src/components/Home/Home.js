2a368b9ef034fb0755ccb2fbebec8773bc4187ca
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 21:51:19 +0100

    Add & Export component Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,2 +1,17 @@
 import React, { Component } from 'react';
 import './Home.css';
+
+class Home extends Component{
+  render(){
+    return(
+      <div>
+        <h2>Witaj!</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit diam massa, quis ornare augue convallis sed. Sed suscipit elementum tristique. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean bibendum orci vitae elementum porttitor. Aliquam erat volutpat. Sed sagittis ipsum id enim ultricies finibus. Aenean consequat elit metus, vel tempus lacus dictum eget. Aliquam vulputate ac ante vitae fringilla. In nisl mauris, dictum sit amet elit vel, posuere facilisis tortor. Nunc condimentum facilisis orci, nec rutrum massa tincidunt vitae. Morbi sed tortor aliquet, finibus odio vel, semper libero. Mauris eu elementum erat. Aenean volutpat, tellus a condimentum tempus, orci nisi sollicitudin quam, quis porttitor tellus ligula in ligula. Nulla facilisi. Fusce mollis ac libero a blandit.</p>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing.</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
