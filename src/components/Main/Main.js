6f5a9a9acd2ddc5118f8f0195d9f614c59492f0e
Stas Miotk <tgb.stas@hotmail.com>
2021-01-09 21:47:06 +0100

    Add content of component Main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,6 +5,15 @@ class Main extends Component{
   render(){
     return(
       <div>
+        <h1>Prosta aplikacja jednostronowa</h1>
+        <ul className="header">
+          <li><a href="/">Strona główna</a></li>
+          <li><a href="/">O nas</a></li>
+          <li><a href="/contact">Kontakt</a></li>
+        </ul>
+        <div className="content">
+
+        </div>
       </div>
     );
   }
